62e0ae6d4a12d9eaaf7b6fe403192f8845c2c4cd
strako <armandoh.ibarra@gmail.com>
2020-09-04 22:56:00 -0500

    primer commit

---
+++ src/sudoku/Sudoku.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Desk-strako
+ */
+public class Sudoku {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int m=3;
+        int n=3;
+        Scanner in = new Scanner(System.in);
+        int mat[][] = new int [m][n];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                System.out.println("ingresa valor en la posicion: "+i+","+j+":");
+                mat[i][j]=in.nextInt();
+            }
+        }
+        
+        for (int i = 0; i < mat.length; i++) {
+            System.out.println(" ");
+            for (int j = 0; j < mat.length; j++) {
+                System.out.print("["+mat[i][j]+"]");
+            }
+        }
+    }
+    
+}
